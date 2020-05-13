795df328e69bfb51cc49b3d9ce19e0d4c5d15a7e
kcboggs <kimberley.cabrera@yahoo.com>
2020-05-13 09:35:27 -0700

    more work in js

--- js/app.js
+++ js/app.js
@@ -1,76 +1,120 @@
 'use strict';
 
-// var parent = document.getElementById('Images');
+var names = [];
+var votes = [];
+var totalVotes = 0;
+var allUniqueImages =[];
+// eslint-disable-next-line no-unused-vars
+var uniqueIndexArray =[];
+var parentElement = document.getElementById('images');
 
-// image array
-var allImages =[];
-
-function UniqueImages(url, alt, title){
-  this.alt =alt;
-  this.title =title;
-  this.filePath = url;
+function UniqueImages(name, extension){
+  this.alt =name;
   this.votes =0;
   this.views =0;
-  UniqueImages.push(this);
+  this.title =name;
+  this.filePath = `images/${name}${extension}`;
+  allUniqueImages.push(this);
 }
-
-var image1 = new UniqueImages('img/bathroom.jpg', 'bathroom', 'bathroom');
-var image2 = new UniqueImages('img/sweep.png', 'sweep', 'sweep');
-var image3 = new UniqueImages('img/usb.gif', 'usb', 'usb');
-
-// rendering random images to the DOM from an array of images
-new UniqueImages('img/bathroom.jpg', 'bathroom', 'bathroom');
-new UniqueImages('img/sweep.png', 'sweep', 'sweep');
-new UniqueImages('img/usb.gif', 'usb', 'usb');
-
-
-UniqueImages.prototype.images = function(){
-  // create and element - use an img
-  var imageElement =document.createElement('img');
-
-  imageElement.setAttribute('src', this.bathroom);
-  imageElement.setAttribute('alt', this.sweep);
-  imageElement.setAttribute('title', this.usb);
-
-  parent.appendChild(imageElement);
+UniqueImages.prototype.render = function(){
+  var imageElement = document.createElement('img');
+  imageElement.src = this.filePath;
+  imageElement.title = this.title;
+  imageElement.alt = this.alt;
+  parentElement.appendChild(imageElement);
 };
 
-image1.appendImage();
-image2.appendImage();
-image3.appendChild();
-
-// helper function will give a random number
-function randomNumber(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+new UniqueImages('bathroom', '.jpg');
+new UniqueImages('breakfast', '.jpg');
+new UniqueImages('cthulhu', '.jpg');
+new UniqueImages('dog-duck', '.jpg');
+new UniqueImages('pen', '.jpg');
+new UniqueImages('sweep', '.png');
 
 function getRandomImage(){
-  parent.textContent = '';
-
-  var randomIndex =randomNumber(0, allImages.length-1);
-  var secondRandomIndex = randomNumber(0, allImages.length-1);
-
-  while(randomIndex === secondRandomIndex){
-    secondRandomIndex = randomNumber(0, allImages.length-1);
+  var uniqueIndexArray = [];
+  var index = getRandomNumber(UniqueImages.length);
+  while(uniqueIndexArray.includes(index)){
+    index = getRandomNumber(allUniqueImages.length);
   }
+  uniqueIndexArray.push(index);
 
-  // object instance to call appendImage function
-  allImages[randomIndex].appendImage();
-  allImages[randomIndex].views++;
-
-  allImages[secondRandomIndex].appendImage();
-  allImages[secondRandomIndex].views++;
+  if(uniqueIndexArray.length > 6){
+    uniqueIndexArray.shift();
+  }
+  return index;
 }
-getRandomImage();
 
-// event listener
-parent.addEventListener('click', function(){
-  var titleOfImageThatWasClickedOn = event.target.title;
+function getRandomNumber(max){
+  return Math.floor(Math.random() * max);
+}
 
-  for(var i =0; i<allImages.length; i++){
-    if(titleOfImageThatWasClickedOn === allImages[i].title){
-      allImages[i].votes++;
+function displayImage(){
+  var index = getRandomImage();
+  allUniqueImages[index].render();
+}
+
+function handleClick(event){
+  parentElement.textContent = '';
+  var titleOfTheImageClickedOn = event.target.title;
+
+  for(var i=0; i<allUniqueImages.length; i++){
+    if(titleOfTheImageClickedOn === allUniqueImages[i].title){
+      allUniqueImages[i].votes++;
+      totalVotes++;
+
+      if(totalVotes === 25){
+        parentElement.removeEventListener('click', handleClick);
+        makeNamesArray();
+      }
     }
   }
-  getRandomImage();
-});
+  displayImage();
+  displayImage();
+  displayImage();
+}
+displayImage();
+displayImage();
+displayImage();
+
+parentElement.addEventListener('click', handleClick);
+
+function makeNamesArray(){
+  for(var i=0; i<allUniqueImages.length; i++){
+    names.push(allUniqueImages[i].title);
+    votes.push(allUniqueImages[i].votes);
+  }
+  generateChart();
+}
+
+function generateChart(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var MyChart = new MyChart(ctx, {
+    type:'bar',
+    data: {
+      labels: names,
+      dataset: [{
+        label: '# of Votes',
+        data: votes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
